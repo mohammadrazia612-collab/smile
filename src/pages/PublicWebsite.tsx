fc73ff4e32761a2b4fe63e10691afd561f166a35
Habeeb <habeeb@users.noreply.github.com>
2026-09-20 10:55:31 +0530

    revert: remove RACHU SMALL T section and associated assets

--- src/pages/PublicWebsite.tsx
+++ src/pages/PublicWebsite.tsx
@@ -16,7 +16,6 @@ import { Technology } from '../components/Technology';
 import { FAQSection } from '../components/FAQSection';
 import { AppointmentSection } from '../components/AppointmentSection';
 import { ContactSection } from '../components/ContactSection';
-import { RachuSmallT } from '../components/RachuSmallT';
 import { Footer } from '../components/Footer';
 
 import { useSmoothScroll } from '../hooks/useSmoothScroll';
@@ -100,10 +99,7 @@ export const PublicWebsite: React.FC = () => {
         {/* 9. Results / Clinical Cases Portfolio */}
         <SmileGallery />
 
-        {/* 10. RACHU SMALL T Feature Showcase */}
-        <RachuSmallT />
-
-        {/* 11. Testimonials */}
+        {/* 10. Testimonials */}
         <Testimonials />
 
         {/* 11. Trust & Key Benefits */}
